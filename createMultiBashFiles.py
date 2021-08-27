7826f2ef8996af95c0b9efb42d779139c5ef2967
Wren <vetens@wisc.edu>
2021-08-27 14:47:19 +0200

    gfal storage interface fixed

--- createMultiBashFiles.py
+++ createMultiBashFiles.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser(description = 'Generate Bash scripts for EPOS-L
 parser.add_argument('--nfiles', type=int, help='Number of HepMC files to create', default=1000)
 parser.add_argument('--nevts', type=int, help='Number of events per file', default=4000)
 parser.add_argument('--scramarch', type=str, help='SCRAM architecture of system', default='slc7_amd64_gcc700')
-parser.add_argument('--workdir', type=str, help='working directory (should be your cmssw/src)', default='/afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src')
+parser.add_argument('--workdir', type=str, help='working directory (should be your cmssw/src)', default='/afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src')
 parser.add_argument('--gfal_prefix', type=str, help='Your T2 prefix for gfal', default='davs://cmsxrootd.hep.wisc.edu:1094')
 parser.add_argument('--trialname', type=str, help='name of trial (note, to change the mass in the simulation in crmc_Sexaq_incl_installed go to crmc.param and at the end of the file add the line "setamhdibar m_x" where m_x is the desire S mass you want to generate', default="27_1p8GeV")
 parser.add_argument('--pwstdin', type=str, help='path of fil with password standin for voms', default="~/pwstdin")
@@ -22,7 +22,7 @@ for i in range(0,args.nfiles):
 	file.write("cd " + args.workdir + " ; eval `scram runtime -sh` ; cd - >/dev/null" + "\n")
 	#proxy to be able to write the files to /pnfs
         file.write("export X509_USER_PROXY=/tmp/x509up_u$(id -u $USER)" + "\n")
-        file.write("voms-proxy-init -pwstdin < " + args.pwstdin + "\n")
+        file.write("voms-proxy-init -voms cms -pwstdin < " + args.pwstdin + "\n")
 	#create dir for output files
 	file.write("cd " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed" + "\n")
 	file.write("mkdir -p " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/" + "\n")
@@ -34,10 +34,10 @@ for i in range(0,args.nfiles):
 	#remove hepmc file
 	file.write("rm " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_" + str(i) + ".hepmc" + "\n")
 	#prepare to move to pnfs
-	file.write("gfal-mkdir " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial"+ args.trialname + " 2>/dev/null"+"\n")
+	file.write("(eval `scram unsetenv -sh`; gfal-mkdir " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_"+ args.trialname + " >/dev/null)"+"\n")
 	#sometimes the copy failed the first time so therefore I put it twice ...
-	file.write("gfal-copy " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_" + str(i) + ".root " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial" + args.trialname + "\n")
-	file.write("gfal-copy " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_" + str(i) + ".root " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial" + args.trialname + "\n")
+	file.write("(eval `scram unsetenv -sh`; gfal-copy " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_" + str(i) + ".root " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_" + args.trialname + "/crmc_Sexaq_" + str(i) + ".root)" + "\n")
+	file.write("(eval `scram unsetenv -sh`; gfal-copy " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_" + str(i) + ".root " + args.gfal_prefix + "/store/user/" + args.user + "/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_" + args.trialname + "/crmc_Sexaq_" + str(i) + ".root)" + "\n")
 	#its now on /pnfs so I can remove
 	file.write("rm " + args.workdir + "/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_" + str(i) + ".root " + "\n")
 	file.close()
--- shell/test.sh
+++ shell/test.sh
@@ -1,14 +1,16 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc700
-cd /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src ; eval `scram runtime -sh` ; cd - >/dev/null
+cd /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src ; eval `scram runtime -sh` ; cd - >/dev/null
 export X509_USER_PROXY=/tmp/x509up_u$(id -u $USER)
-voms-proxy-init -pwstdin < ~/pwstdin
-cd /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed
-mkdir /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/
-mkdir /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/
-/afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/bin/crmc -S13000 -n10 -m0 -f /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_0.hepmc
-cmsRun /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/IOMC/Input/test/hepmc2gen.py print inputFiles=file:/afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_0.hepmc outputFile=file:/afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_test.root
-gfal-mkdir davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial27_1p8GeV 2>/dev/null
-gfal-copy /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial27_1p8GeV
-gfal-copy /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial27_1p8GeV
-#rm /afs/cern.ch/work/w/wvetens/Sexaquarks/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root 
+voms-proxy-init -voms cms -pwstdin < ~/pwstdin
+cd /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed
+mkdir -p /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/
+mkdir -p /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/
+/afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/bin/crmc -S13000 -n10 -m0 -f /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_0.hepmc
+cmsRun /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/IOMC/Input/test/hepmc2gen.py print inputFiles=file:/afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_0.hepmc outputFile=file:/afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root
+rm /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/tmp/crmc_Sexaq_0.hepmc
+
+(eval `scram unsetenv -sh`; gfal-mkdir davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_test_1p8GeV >/dev/null)
+(eval `scram unsetenv -sh`; gfal-copy /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_test_1p8GeV/crmc_Sexaq_0.root)
+(eval `scram unsetenv -sh`; gfal-copy /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root davs://cmsxrootd.hep.wisc.edu:1094/store/user/wvetens/crmc_Sexaq/crmc/Sexaquark_13TeV_trial_test_1p8GeV/crmc_Sexaq_0.root)
+rm /afs/cern.ch/work/w/wvetens/Sexaquarks/tmp/CMSSW_10_2_26/src/CustomGenerator/Custom/crmc_Sexaq_incl_installed/SexaquarkProduction/OutputRootFile/OutputRootFile_with_Vtx_smearing2/crmc_Sexaq_0.root 
